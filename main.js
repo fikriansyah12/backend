9ddd6f315b844dda5cc2f142939a8aac10dca4a0
fikriansyah12 <fikriansyahmuhammad664.com>
2024-05-11 01:22:20 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { createConnection } from 'mysql';
+const app = express()
+const port =3306
+
+const con = createConnection({
+    host: "localhost",
+    root : "root",
+    password: "",
+    database: "users",
+});
+
+con.connect ((err) =>{
+    if(err)throw err;
+    console.log("Database Connected");
+});
+app.get('/', (req,res) => {
+    database.query("SELECT * FROM users", (err, rows) => {
+if (err)throw err;
+console.error(err.stack)
+nextTick(err);
+res.json({
+succes: true,
+messages: "getting user",
+data :rows,
+})
+    })
+   
+});
+app.listen(3306,()=>{
+    console.log('example app listening on port ${3306}')
+})
